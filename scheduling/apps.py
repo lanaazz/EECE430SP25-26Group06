5cecb43fc1c44c93e60d4119aec98d0793ab459c
mirakhalil1 <107476937+mirakhalil1@users.noreply.github.com>
2026-04-23 12:36:33 +0300

    Delete scheduling directory

--- scheduling/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SchedulingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scheduling'
-
-    def ready(self):
-        import scheduling.signals
